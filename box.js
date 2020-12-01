d359e9d6abf95ceac9707d9af380a300cd606f1d
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2020-11-30 17:26:15 -0700

    Add files via upload

--- box.js
+++ box.js
@@ -1,5 +1,6 @@
 class Box{
     constructor(x, y) {
+
       var options = {
        isStatic: false,
        restitution: 0,
@@ -9,15 +10,29 @@ class Box{
       this.body = Bodies.rectangle(x, y, 30, 40, options);
       this.width = 30;
       this.height = 40;
+      this.Visiblity = 255;
       World.add(world, this.body);
     }
     display(){
 
 
-        var pos = this.body.position
+        var pos = this.body.position;
         rectMode(CENTER);
         fill("red");
         rect(pos.x, pos.y, this.width, this.height);
+        
+        if(this.body.speed < 3){
+          
       
+         }
+         else{
+           World.remove(world, this.body);
+           push();
+           var pos = this.body.position;
+           this.Visibility = this.Visibility - 5;
+           tint(255,this.Visibility);
+           rect();
+           pop();
+         }
       }
   };
