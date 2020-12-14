434ce9cd0ce3072b820ada2753816c2a5ad02eb0
rohit-Ironman <72317546+rohit-Ironman@users.noreply.github.com>
2020-12-14 07:52:21 -0700

    Add files via upload

--- box.js
+++ box.js
@@ -15,26 +15,29 @@ class Box{
     }
     display(){
 
+      rectMode(CENTER); 
+      fill("red"); 
+      if(this.body.speed < 3){
+        var angle = this.body.angle; 
+        var pos = this.body.position; 
+        push();
+        translate(pos.x, pos.y); 
+        rotate(angle); 
+        rectMode(CENTER); 
+        rect(0, 0, this.width, this.height); 
+        pop(); 
 
-        var pos = this.body.position;
-        rectMode(CENTER);
-        fill("red");
-        rect(pos.x, pos.y, this.width, this.height);
-        
-        if(this.body.speed < 3){
-          
-      
-         }
-         else{
-           World.remove(world, this.body);
-           push();
-           var pos = this.body.position;
-           this.Visibility = this.Visibility - 5;
-           tint(255,this.Visibility);
-           score++
-           rect();
-           pop();
-         }
+      }
+      else{
+        World.remove(world, this.body); 
+        push(); 
+        this.Visibility =this.Visibility - 5; 
+        tint(255, this.Visibility); 
+        score++;
+         rect();
+         pop();
+
+      }
 
       }
   };
